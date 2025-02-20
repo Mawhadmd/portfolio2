3bbd800931123c1e8b8bfe69b49959c626148c59
Mohammed <104397399+Mawhadmd@users.noreply.github.com>
2025-02-20 21:13:14 +0200

    fix: update Pulsingdot component styles for better color handling; adjust color intensity based on theme and song status; update package dependencies for @types/node

--- src/components/pulsingdot.tsx
+++ src/components/pulsingdot.tsx
@@ -1,31 +1,31 @@
 
 
 const Pulsingdot = ({song, CurrentlyPlaying, ThemeColor}: {song:string; CurrentlyPlaying: `Yes` | "No",  ThemeColor: 'light' | 'dark';}) => {
-    let colorIntensity = ThemeColor === 'dark'? 300: 600
+    let colorIntensity = ThemeColor === 'dark'? 255: 255
     return (
         <div
-          className="relative size-2 rounded-full mx-auto"
-          style={{
-            backgroundColor:
-              song === "Error"
-                ? `rgb(${colorIntensity}, 0, 0)`
-                : CurrentlyPlaying === "Yes"
-                ? `rgb(0, ${colorIntensity}, 0)`
-                : `rgb(${colorIntensity}, ${colorIntensity}, 0)`,
-          }}
-        >
-          <div
-            className="absolute animate-ping -z-10 inset-0 size-2 rounded-full mx-auto"
-            style={{
-              backgroundColor:
-                song === "Error"
-                  ? `rgba(${colorIntensity}, 0, 0, 0.5)`
-                  : CurrentlyPlaying === "Yes"
-                  ? `rgba(0, ${colorIntensity}, 0, 0.5)`
-                  : `rgba(${colorIntensity}, ${colorIntensity}, 0, 0.5)`,
-            }}
-          ></div>
-        </div>
+              className="relative size-2 rounded-full mx-auto"
+              style={{
+                backgroundColor:
+                  song === "Error"
+                    ? `rgb(${colorIntensity}, 100, 0)`
+                    : CurrentlyPlaying === "Yes"
+                    ? `rgb(0, ${colorIntensity}, 0)`
+                    : `rgb(${colorIntensity}, ${colorIntensity-80}, 0)`,
+              }}
+            >
+              <div
+                className="absolute animate-ping -z-10 inset-0 size-2 rounded-full mx-auto"
+                style={{
+                  backgroundColor:
+                    song === "Error"
+                      ? `rgb(${colorIntensity}, 0, 0)`
+                      : CurrentlyPlaying === "Yes"
+                      ? `rgb(0, ${colorIntensity}, 0)`
+                      : `rgb(${colorIntensity}, ${colorIntensity-80}, 0)`,
+                }}
+              ></div>
+            </div>
     );
 }
 
