f40f0b28c36268a5787dcc1e71609f75c24cafff
Mohammed <104397399+Mawhadmd@users.noreply.github.com>
2025-02-22 17:01:53 +0200

    feat: enhance LandingCard component with motion effects on profile image hover

--- Client/src/components/Landing/LandingCard.tsx
+++ Client/src/components/Landing/LandingCard.tsx
@@ -1,3 +1,4 @@
+import { motion } from "motion/react";
 import pfp from "../../assets/MyPicture.jpg";
 import Blurfade from "../ui/Blurfade";
 import AboutMe from "./AboutMe";
@@ -7,10 +8,11 @@ const MainCard = () => {
   return (
   <>
       <div  className="aspect-square h-20">
-        <img
+        <motion.img
+        whileHover={{ scale: 1.4, rotate: 10, filter: "blur(0)" }}
           src={pfp}
           alt=""
-          className="blur-sm size-20 border-border rounded-full aspect-square"
+          className="blur-xs size-20 border-border rounded-full aspect-square"
         />
       </div>
       <div className="mt-6 space-y-4">
